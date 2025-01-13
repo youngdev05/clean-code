2f1a0bd5965353e3fd6d733cff5ad789dc003604
Dmitry <truckcyber@yandex.ru>
2025-01-13 11:36:09 +0300

    models, infrastructure and db added

---
+++ Markdown/MdTest/UnitTest1.cs
@@ -0,0 +1,9 @@
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void SingleUnderscore_AppliesItalics()
+    {
+    }
+}
---
+++ WebApp/WebApp/Persistence/Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureDatabase(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(connectionString)); // Используем PostgreSQL
+        }
+    }
+}
